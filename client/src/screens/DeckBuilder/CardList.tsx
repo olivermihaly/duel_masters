b7f81fa2fae4e64b416543613392146ef6815cc7
Oliver Mihaly <oliver@ecven.com>
2024-06-14 22:02:00 +0200

    duel masters progress draggable cards

--- client/src/screens/DeckBuilder/CardList.tsx
+++ client/src/screens/DeckBuilder/CardList.tsx
@@ -1,5 +1,5 @@
 import Card from "../../components/Card";
-import ListCard from "./ListCard";
+import ListCard, { DraggableListCard } from "./ListCard";
 import PanelHeader from "./PanelHeader";
 import image from "../../assets/cards/aqua_knight.png";
 import image2 from "../../assets/cards/aqua_hulcus.png";
@@ -15,26 +15,28 @@ import image11 from "../../assets/cards/bolshack_dragon.png";
 import image12 from "../../assets/cards/unicorn_fish.png";
 import image13 from "../../assets/cards/urth_purifying_elemental.png";
 import image14 from "../../assets/cards/artisan_picora.png";
+import image15 from "../../assets/newCards/015fd6bb-37a9-45cf-bb6b-a5497412b880.jpg";
 
 export default function CardList() {
   return (
     <div className="card-list">
       <PanelHeader title="Card List" />
       <div className="card-list-cards">
-        <ListCard src={image} />
-        <ListCard src={image2} />
-        <ListCard src={image3} />
-        <ListCard src={image4} />
-        <ListCard src={image5} />
-        <ListCard src={image6} />
-        <ListCard src={image7} />
-        <ListCard src={image8} />
-        <ListCard src={image9} />
-        <ListCard src={image10} />
-        <ListCard src={image11} />
-        <ListCard src={image12} />
-        <ListCard src={image13} />
-        <ListCard src={image14} />
+        <DraggableListCard src={image} />
+        <DraggableListCard src={image2} />
+        <DraggableListCard src={image3} />
+        <DraggableListCard src={image4} />
+        <DraggableListCard src={image5} />
+        <DraggableListCard src={image6} />
+        <DraggableListCard src={image7} />
+        <DraggableListCard src={image8} />
+        <DraggableListCard src={image9} />
+        <DraggableListCard src={image10} />
+        <DraggableListCard src={image11} />
+        <DraggableListCard src={image12} />
+        <DraggableListCard src={image13} />
+        <DraggableListCard src={image14} />
+        <DraggableListCard src={image15} />
       </div>
     </div>
   );
--- client/src/screens/DeckBuilder/ListCard.tsx
+++ client/src/screens/DeckBuilder/ListCard.tsx
@@ -1,6 +1,7 @@
+import { useRef, useState, CSSProperties, useEffect } from "react";
 import useMyDecksStore from "../../stores/useMyDeckStore";
 
-export default function ListCard({src}:{src:string}) {
+export default function ListCard({ src }: { src: string }) {
   const [addCardToDeck] = useMyDecksStore((state) => [state.addCardToDeck]);
 
   return (
@@ -9,5 +10,105 @@ export default function ListCard({src}:{src:string}) {
       src={src}
       onClick={() => addCardToDeck({ src: src })}
     ></img>
+  );
+}
+
+type DraggableListCardProps = {
+  src: string;
+};
+
+type DraggableImageProps = {
+  src: string;
+  style: CSSProperties;
+  removeCard: () => void;
+};
+
+export function DraggableListCard({ src }: DraggableListCardProps) {
+  const cardRef = useRef<HTMLImageElement>(null);
+  const [draggableCard, setDraggableCard] =
+    useState<DraggableImageProps | null>(null);
+
+  const handleClick = (e: MouseEvent) => {
+    const rect = cardRef.current?.getBoundingClientRect();
+    // img.style.left = `${e.clientX - 0}px`;
+    //  img.style.top = `${e.clientY - 0}px`;
+    if (rect) {
+      setDraggableCard({
+        src,
+        style: {
+          position: "absolute",
+          left: `${e.clientX}px`,
+          top: `${e.clientY}px`,
+          borderRadius: 10,
+          cursor: "pointer",
+          transform: "translate(-50%,-50%)",
+          zIndex: 1,
+        },
+        removeCard: () => setDraggableCard(null),
+      });
+    }
+  };
+
+  return (
+    <div className="draggable-list-card-container" draggable={false}>
+      <img
+        ref={cardRef}
+        className="list-card"
+        style={{ borderRadius: 10 }}
+        src={src}
+        onMouseDown={(e) => handleClick(e)}
+        draggable={false}
+      ></img>
+      {draggableCard && (
+        <DraggableImage
+          src={draggableCard.src}
+          style={draggableCard.style}
+          removeCard={() => setDraggableCard(null)}
+        />
+      )}
+    </div>
+  );
+}
+function DraggableImage({ src, style, removeCard }: DraggableImageProps) {
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [addCardToDeck] = useMyDecksStore((state) => [state.addCardToDeck]);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    return function cleanup() {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    };
+  }, []);
+
+  function onMouseUp(e: MouseEvent) {
+    let container = document.getElementsByClassName("my-decks")[0];
+    let rect = container.getBoundingClientRect();
+    if (rect.x < e.clientX && rect.y < e.clientY) {
+      addCardToDeck({ src: src });
+      removeCard();
+    } else {
+      removeCard();
+    }
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    //console.log(e.clientY, e.clientX);
+    const img = imgRef.current;
+    if (img) {
+      img.style.left = `${e.clientX - 0}px`;
+      img.style.top = `${e.clientY - 0}px`;
+    }
+  };
+
+  return (
+    <img
+      ref={imgRef}
+      src={src}
+      className="draggable-list-card"
+      style={style}
+      draggable={false}
+    />
   );
 }
